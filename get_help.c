95b376dcafd73d844be096b5d74c632619fdb57a
Gafar H Salam <aygafsam2020@gmail.com>
2022-11-16 03:18:22 -0800

    simple shell

--- get_help.c
+++ get_help.c
@@ -8,29 +8,27 @@
  */
 int exit_shell(data_shell *datash)
 {
-        unsigned int ustatus;
-        int is_digit;
-        int str_len;
-        int big_number;
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
 
-        if (datash->args[1] != NULL)
-        {
-                ustatus = _atoi(datash->args[1]);
-                is_digit = _isdigit(datash->args[1]);
-                str_len = _strlen(datash->args[1]);
-                big_number = ustatus > (unsigned int)INT_MAX;
-                if (!is_digit || str_len > 10 || big_number)
-                {
-                        get_error(datash, 2);
-                        datash->status = 2;
-                        return (1);
-                }
-                datash->status = (ustatus % 256);
-        }
-        return (0);
+	if (datash->args[1] != NULL)
+	{
+		ustatus = _atoi(datash->args[1]);
+		is_digit = _isdigit(datash->args[1]);
+		str_len = _strlen(datash->args[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(datash, 2);
+			datash->status = 2;
+			return (1);
+		}
+		datash->status = (ustatus % 256);
+	}
+	return (0);
 }
-root@GreenWater:/mnt/c/Users/Ayomide GreenWater/Desktop/alx/simple_shell_test# cat get_help.c
-#include "shell.h"
 
 /**
  * get_help - function that retrieves help messages according builtin
@@ -40,26 +38,26 @@ root@GreenWater:/mnt/c/Users/Ayomide GreenWater/Desktop/alx/simple_shell_test# c
 int get_help(data_shell *datash)
 {
 
-        if (datash->args[1] == 0)
-                aux_help_general();
-        else if (_strcmp(datash->args[1], "setenv") == 0)
-                aux_help_setenv();
-        else if (_strcmp(datash->args[1], "env") == 0)
-                aux_help_env();
-        else if (_strcmp(datash->args[1], "unsetenv") == 0)
-                aux_help_unsetenv();
-        else if (_strcmp(datash->args[1], "help") == 0)
-                aux_help();
-        else if (_strcmp(datash->args[1], "exit") == 0)
-                aux_help_exit();
-        else if (_strcmp(datash->args[1], "cd") == 0)
-                aux_help_cd();
-        else if (_strcmp(datash->args[1], "alias") == 0)
-                aux_help_alias();
-        else
-                write(STDERR_FILENO, datash->args[0],
-                      _strlen(datash->args[0]));
+	if (datash->args[1] == 0)
+		aux_help_general();
+	else if (_strcmp(datash->args[1], "setenv") == 0)
+		aux_help_setenv();
+	else if (_strcmp(datash->args[1], "env") == 0)
+		aux_help_env();
+	else if (_strcmp(datash->args[1], "unsetenv") == 0)
+		aux_help_unsetenv();
+	else if (_strcmp(datash->args[1], "help") == 0)
+		aux_help();
+	else if (_strcmp(datash->args[1], "exit") == 0)
+		aux_help_exit();
+	else if (_strcmp(datash->args[1], "cd") == 0)
+		aux_help_cd();
+	else if (_strcmp(datash->args[1], "alias") == 0)
+		aux_help_alias();
+	else
+		write(STDERR_FILENO, datash->args[0],
+				_strlen(datash->args[0]));
 
-        datash->status = 0;
-        return (1);
+	datash->status = 0;
+	return (1);
 }
--- main.c
+++ main.c
@@ -8,15 +8,15 @@
  */
 void free_data(data_shell *datash)
 {
-        unsigned int i;
+	unsigned int i;
 
-        for (i = 0; datash->_environ[i]; i++)
-        {
-                free(datash->_environ[i]);
-        }
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
 
-        free(datash->_environ);
-        free(datash->pid);
+	free(datash->_environ);
+	free(datash->pid);
 }
 
 /**
@@ -28,26 +28,26 @@ void free_data(data_shell *datash)
  */
 void set_data(data_shell *datash, char **av)
 {
-        unsigned int i;
+	unsigned int i;
 
-        datash->av = av;
-        datash->input = NULL;
-        datash->args = NULL;
-        datash->status = 0;
-        datash->counter = 1;
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
 
-        for (i = 0; environ[i]; i++)
-                ;
+	for (i = 0; environ[i]; i++)
+		;
 
-        datash->_environ = malloc(sizeof(char *) * (i + 1));
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
 
-        for (i = 0; environ[i]; i++)
-        {
-                datash->_environ[i] = _strdup(environ[i]);
-        }
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
 
-        datash->_environ[i] = NULL;
-        datash->pid = aux_itoa(getpid());
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
 }
 
 /**
@@ -60,14 +60,14 @@ void set_data(data_shell *datash, char **av)
  */
 int main(int ac, char **av)
 {
-        data_shell datash;
-        (void) ac;
+	data_shell datash;
+	(void) ac;
 
-        signal(SIGINT, get_sigint);
-        set_data(&datash, av);
-        shell_loop(&datash);
-        free_data(&datash);
-        if (datash.status < 0)
-                return (255);
-        return (datash.status);
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
